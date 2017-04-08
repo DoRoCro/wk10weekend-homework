5addf6c50cbeb85132b391ae4e6178f283175983
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-04-08 13:14:00 +0100

    add buyRecord method to customer

--- record_store/customer.js
+++ record_store/customer.js
@@ -4,4 +4,20 @@ var Customer = function (name, starterCash) {
   this.collection = []
 }
 
+Customer.prototype = {
+  buyRecord: function (record) {
+    // consumes record if bought, else returns it
+    // experiment with mixed reutrn types...
+    if (this.canBuyRecord(record)) {
+      this.collection.push(record)
+      this.cash -= record.price
+      return true
+    }
+    return record
+  },
+  canBuyRecord: function (record) {
+    return (this.cash >= record.price)
+  }
+}
+
 module.exports = Customer
--- record_store/specs/customer_spec.js
+++ record_store/specs/customer_spec.js
@@ -1,9 +1,18 @@
 var Customer = require('../customer.js')
 var a = require('assert')
+var Record = require('../record.js')
 
-var customer1 = new Customer('The Record Collector', 100)
+var customer1
+var disc1
+var unaffordableDisc2
 
 describe('Customer', function () {
+  beforeEach('Before Each:', function () {
+    customer1 = new Customer('The Record Collector', 100)
+    disc1 = new Record('Half Man, Half Biscuit', 'Westward Ho! Massive Letdown', 'post-punk', 1.99)
+    unaffordableDisc2 = new Record('The Beatles', 'First Pressing Vintage Love Me Do', '60s', 5010.50)
+  })
+
   it('has name', function () {
     a.strictEqual(customer1.name, 'The Record Collector')
   })
@@ -14,5 +23,12 @@ describe('Customer', function () {
 
   it('has a collection', function () {
     a.deepEqual(customer1.collection, [])
+  })
+
+  it('can buy within available cash', function () {
+    customer1.buyRecord(disc1)
+    a.strictEqual(customer1.cash, 98.01)
+    customer1.buyRecord(unaffordableDisc2)
+    a.strictEqual(customer1.cash, 98.01)
   })
 })
